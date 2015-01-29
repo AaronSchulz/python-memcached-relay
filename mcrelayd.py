f07c7de29106b9dd395cf8d8dd44e70f2d0a9ceb
Aaron Schulz <aschulz@wikimedia.org>
2015-01-28 17:20:19 -0800

    Some cleanups after testing

--- mcrelayd.py
+++ mcrelayd.py
@@ -5,49 +5,66 @@ import redis
 import memcache
 import time
 
-def main()
+def main():
 	parser = argparse.ArgumentParser(description='Process memcached commands from a channel')
-	parser.add_argument('--config-file', help='YAML configuration file')
+	parser.add_argument('--config-file', required=True, help='YAML configuration file')
 	args = parser.parse_args()
+
 	# Load the config file
+	print "Loading YAML config..."
 	f = open(args.config_file)
 	config = yaml.safe_load(f)
 	f.close()
+
 	# Connect to memcached server
-	mc = memcache.Client([config.memcached_server])
+	# @note: the client library already handle re-connection logic
+	print "Connecting to memcached server %s..." % config['memcached_server']
+	mc = memcache.Client([config['memcached_server']])
+
 	# Connect to the redis PubSub servers
+	# @fixme: disconnects stop the daemon
 	redisMap = {}
 	pubSubMap = {}
-	for redis_host in config.redis_hosts:
-		redisMap[redis_host] = redis.StrictRedis(host=redis_host, port=config.redis_port, password=config.redis_password)
+	for redis_host in config['redis_hosts']:
+		print "Connecting to redis server %s on %s..." % (redis_host, config['redis_port'])
+		redisMap[redis_host] = redis.StrictRedis(host=redis_host, port=config['redis_port'], password=config['redis_password'])
 		pubSubMap[redis_host] = redisMap[redis_host].pubsub()
-		pubSubMap[redis_host].subscribe(config.redis_channel)
+		print "Subscribing to channel %s" % config['redis_channel']
+		pubSubMap[redis_host].subscribe(config['redis_channel'])
+
 	# Stream in updates from the channel on all servers indefinetely
+	print "Listening for channel events..."
 	while True:
 		wait = True
-		# Iterate through each host serving the channel (HA)
+		# Iterate through each host serving the channel
 		for redis_host in pubSubMap:
+			# Process the next message if one is ready
 			event = pubSubMap[redis_host].get_message()
 			if event and event['type'] == 'message':
 				doUpdate(mc, event['data'])
 				wait = False
-		# Slow down if nothing was found this round
+		# Avoid high CPU usage
 		if wait:
-			time.sleep(0.001)
+			time.sleep(0.005)
 
 def doUpdate(mc, message):
 	command = json.loads(message)
-	if command.cmd == 'set':
-		mc.set(command.key, command.value, command.ttl)
-		print 'Applied %s command key "%s".' % (command.cmd, command.key)
-	elif command.cmd == 'add':
-		mc.add(command.key, command.value, command.ttl)
-		print 'Applied %s command key "%s".' % (command.cmd, command.key)
-	elif command.cmd == 'delete':
-		mc.delete(command.key)
-		print 'Applied %s command key "%s".' % (command.cmd, command.key)
+	# @fixme: memcached flag handling
+	ok = True
+	cmd = str(command['cmd']) # commands are always ASCII
+	key = str(command['key']) # keys are always ASCII
+	if cmd == 'set':
+		mc.set(key, command['value'], command['ttl'])
+	elif cmd == 'add':
+		mc.add(key, command['value'], command['ttl'])
+	elif cmd == 'delete':
+		mc.delete(key)
 	else:
-		print 'Got unrecognized command "%s".' % command.cmd
+		ok = False
+		print 'Got unrecognized command "%s"' % cmd
+
+	if ok:
+		print 'Applied %s command on key "%s" to memcached' % (cmd, key)
 
 if __name__ == '__main__':
 	main()
