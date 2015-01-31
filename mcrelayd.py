59205691ceeb99f44f4c7b92ab14d6a358cf15bb
Aaron Schulz <aschulz@wikimedia.org>
2015-01-30 17:13:56 -0800

    Abort when commands memcached fail and handle redis failures

--- mcrelayd.py
+++ mcrelayd.py
@@ -1,9 +1,9 @@
 import argparse
-import yaml
 import json
 import time
 import socket
 
+import yaml
 import redis
 
 def main():
@@ -20,19 +20,29 @@ def main():
 	# Connect to memcached server
 	print("Connecting to memcached server %s:%s..." % (config['memcached_host'],config['memcached_port']))
 	mcSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	#mcSocket.connect(('127.0.0.1', config['memcached_port']))
 	mcSocket.connect((config['memcached_host'], config['memcached_port']))
 
+	retryTTL = 5
+
 	# Connect to the redis PubSub servers
-	# @fixme: disconnects stop the daemon
 	redisMap = {}
 	pubSubMap = {}
+	failedMap = {}
 	for redis_host in config['redis_hosts']:
-		print("Connecting to redis server %s on %s..." % (redis_host, config['redis_port']))
-		redisMap[redis_host] = redis.StrictRedis(host=redis_host, port=config['redis_port'], password=config['redis_password'])
+		# Construct the redis handle (connection is deferred)
+		redisMap[redis_host] = redis.StrictRedis(
+			host=redis_host, port=config['redis_port'],
+			password=config['redis_password'],
+			socket_connect_timeout=2,
+			socket_timeout=2)
 		pubSubMap[redis_host] = redisMap[redis_host].pubsub()
-		print("Subscribing to channel %s" % config['redis_channel'])
-		pubSubMap[redis_host].subscribe(config['redis_channel'])
+		print("Subscribing to channel %s on %s:%s" % (config['redis_channel'],redis_host, config['redis_port']))
+		# Actually connect and subscribe (connection is on first command)
+		try:
+			pubSubMap[redis_host].subscribe(config['redis_channel'])
+		except Exception as e:
+			failedMap[redis_host] = time.time()
+			print("Error connected to redis server %s on %s..." % (redis_host, config['redis_port']))
 
 	# Stream in updates from the channel on all servers indefinetely
 	print("Listening for channel events...")
@@ -40,11 +50,23 @@ def main():
 		wait = True
 		# Iterate through each host serving the channel
 		for redis_host in pubSubMap:
-			# Process the next message if one is ready
-			event = pubSubMap[redis_host].get_message()
-			if event and event['type'] == 'message':
-				relayCommand(mcSocket, event['data'])
-				wait = False
+			try:
+				# Avoid down servers but re-connect periodically if possible
+				if redis_host in failedMap:
+					if (time.time() - failedMap[redis_host]) >= retryTTL:
+						print("Re-subscribing to channel %s on %s:%s" % (config['redis_channel'],redis_host, config['redis_port']))
+						pubSubMap[redis_host].subscribe(config['redis_channel'])
+						del failedMap[redis_host]
+					else:
+						continue
+				# Process the next message if one is ready
+				event = pubSubMap[redis_host].get_message()
+				if event and event['type'] == 'message':
+					relayCommand(mcSocket, event['data'])
+					wait = False
+			except Exception as e:
+				failedMap[redis_host] = time.time()
+				print("Error contacting redis server %s on %s..." % (redis_host, config['redis_port']))
 		# Avoid high CPU usage
 		if wait:
 			time.sleep(0.005)
@@ -67,16 +89,22 @@ def relayCommand(mcSocket, message):
 
 	# Issue the full command
 	mcSocket.sendall(mcCommand)
-	# Get the response status
+	# Get the response status (terminated with \r\n)
 	result = ''
 	while True:
 		c = mcSocket.recv(1)
-		if c == '\n' or c == '':
+		if c == '\r' or c == '':
 			break
 		else:
 			result += c
+	mcSocket.recv(1) # consume \n
 
-	print('Get result: %s' % result)
+	# Check if the response was OK
+	if result in ['STORED','NOT_STORED','DELETED','NOT_FOUND']:
+		print('Got OK result: %s' % result)
+	else:
+		raise Exception('BadMemcachedResult', 'Got bad result: %s' % result)
+
 
 if __name__ == '__main__':
 	main()
