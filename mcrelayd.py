c50564be352a863998b8ea73b6bbae5520d7ba38
Aaron Schulz <aschulz@wikimedia.org>
2015-02-02 16:50:13 -0800

    Added stream replay and made various fixes

--- mcrelayd.py
+++ mcrelayd.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import time
 import socket
+import os
 
 import yaml
 import redis
@@ -17,91 +18,165 @@ def main():
 	config = yaml.safe_load(f)
 	f.close()
 
-	# Connect to memcached server
-	print("Connecting to memcached server %s:%s..." % (config['memcached_host'],config['memcached_port']))
-	mcSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	mcSocket.connect((config['memcached_host'], config['memcached_port']))
+	config['retry_timeout'] = 5 # time to treat servers as down
+	config['pos_write_delay'] = 1 # write positions this often
 
-	retryTTL = 5
+	# Connect to the memcached server
+	print("Connecting to memcached server %s:%s..." % (
+		config['memcached_host'],config['memcached_port']))
+	mc_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	mc_sock.connect((config['memcached_host'], config['memcached_port']))
+	print("Connected")
 
-	# Connect to the redis PubSub servers
-	redisMap = {}
-	pubSubMap = {}
-	failedMap = {}
-	for redis_host in config['redis_hosts']:
+	rd_handles = {}
+	rd_ps_handles = {}
+	rd_fail_times = {}
+	last_pos_write = {}
+
+	# Connect to all the redis PubSub servers...
+	for rd_host in config['redis_hosts']:
 		# Construct the redis handle (connection is deferred)
-		redisMap[redis_host] = redis.StrictRedis(
-			host=redis_host, port=config['redis_port'],
+		rd_handles[rd_host] = redis.StrictRedis(
+			host=rd_host, port=config['redis_port'],
 			password=config['redis_password'],
 			socket_connect_timeout=2,
 			socket_timeout=2)
-		pubSubMap[redis_host] = redisMap[redis_host].pubsub()
-		print("Subscribing to channel %s on %s:%s" % (config['redis_channel'],redis_host, config['redis_port']))
+		# Create the PubSub object (connection is deferred)
+		rd_ps_handles[rd_host] = rd_handles[rd_host].pubsub()
 		# Actually connect and subscribe (connection is on first command)
 		try:
-			pubSubMap[redis_host].subscribe(config['redis_channel'])
-		except Exception as e:
-			failedMap[redis_host] = time.time()
-			print("Error connected to redis server %s on %s..." % (redis_host, config['redis_port']))
+			# Sync from the reliable stream to the bulk of events
+			resyncViaRedisStream(mc_sock, rd_host, rd_handles, time.time(), config)
+			# Subscribe to channel to avoid polling overhead
+			print("Subscribing to channel %s on %s:%s" % (
+				config['redis_channel'],rd_host, config['redis_port']))
+			rd_ps_handles[rd_host].subscribe(config['redis_channel'])
+			print("Subscribed")
+			# Quikly resync to avoid any stream gaps (replay a few things twice)
+			resyncViaRedisStream(mc_sock, rd_host, rd_handles, time.time(), config)
+		except redis.RedisError as e:
+			rd_fail_times[rd_host] = time.time()
+			print("Error contacting redis server %s" % rd_host)
+		# Track the last time the position file was updated for this server
+		last_pos_write[rd_host] = time.time()
 
-	# Stream in updates from the channel on all servers indefinetely
+	# Stream in updates from the channel on all servers indefinitely...
 	print("Listening for channel events...")
 	while True:
 		wait = True
 		# Iterate through each host serving the channel
-		for redis_host in pubSubMap:
+		for rd_host in rd_ps_handles:
 			try:
 				# Avoid down servers but re-connect periodically if possible
-				if redis_host in failedMap:
-					if (time.time() - failedMap[redis_host]) >= retryTTL:
-						print("Re-subscribing to channel %s on %s:%s" % (config['redis_channel'],redis_host, config['redis_port']))
-						pubSubMap[redis_host].subscribe(config['redis_channel'])
-						del failedMap[redis_host]
-					else:
-						continue
+				periodicRedisPing(
+					mc_sock, rd_host, rd_handles, rd_ps_handles, rd_fail_times, config)
 				# Process the next message if one is ready
-				event = pubSubMap[redis_host].get_message()
+				event = rd_ps_handles[rd_host].get_message()
+				# @note: events are of the format <UNIX timetamp>:<JSON>
 				if event and event['type'] == 'message':
-					relayCommand(mcSocket, event['data'])
 					wait = False
-			except Exception as e:
-				failedMap[redis_host] = time.time()
-				print("Error contacting redis server %s on %s..." % (redis_host, config['redis_port']))
+					try:
+						eTime, eMsg = event['data'].split(":",1)
+						command = json.loads(eMsg)
+					except ValueError as e:
+						print("Cannot relay command; invalid JSON")
+						continue
+					# Replicate the update to the memcached server
+					relayMemcacheCommand(mc_sock, command)
+					# Periodically update the position
+					cur_timestamp = time.time();
+					if (cur_timestamp - last_pos_write[rd_host]) > config['pos_write_delay']:
+						info = {'pos': float(eTime)}
+						setCurrentPos(rd_host, info, config)
+						last_pos_write[rd_host] = cur_timestamp
+			except redis.RedisError as e:
+				rd_fail_times[rd_host] = time.time()
+				print("Error contacting redis server %s" % rd_host)
 		# Avoid high CPU usage
 		if wait:
 			time.sleep(0.005)
 
-def relayCommand(mcSocket, message):
-	command = json.loads(message)
+def periodicRedisPing(mc_sock, rd_host, rd_handles, rd_ps_handles, rd_fail_times, config):
+	if not rd_host in rd_fail_times:
+		return
+	if (time.time() - rd_fail_times[rd_host]) >= config['retry_timeout']:
+		# Resubscribe before resync to avoid stream gaps
+		print("Re-subscribing to channel %s on %s:%s" % (
+			config['redis_channel'],rd_host, config['redis_port']))
+		rd_ps_handles[rd_host].subscribe(config['redis_channel'])
+		del rd_fail_times[rd_host]
+		print("Subscribed")
+		# Resync from the reliable stream (replay a few things twice)
+		resyncViaRedisStream(mc_sock, rd_host, rd_handles, time.time(), config)
 
+def resyncViaRedisStream(mc_sock, rd_host, rd_handles, stopPos, config):
+	# Prefix the channel to get the stream key
+	key = "z-stream:%s" % config['redis_channel']
+
+	print("Applying updates from redis server %s" % rd_host);
+
+	# Get the current position time
+	info = getCurrentPos(rd_host, config)
+	# Adjust time range to handle any clock skew
+	clockSkewFuzz = 5
+	info['pos'] = max( 0, info['pos'] - clockSkewFuzz )
+	stopPos = stopPos + clockSkewFuzz
+
+	batchSize = 100
+	print("Starting from position %.6f; ending on %.6f" % (info['pos'],stopPos))
+	# Replicate from the log in batches...
+	while True:
+		events = rd_handles[rd_host].zrangebyscore(
+			key, info['pos'], stopPos, start=0, num=batchSize)
+		# @note: events are of the format <UNIX timetamp>:<JSON>
+		for event in events:
+			try:
+				eTime, eMsg = event.split(":", 1)
+				command = json.loads(eMsg)
+			except ValueError as e:
+				print("Cannot relay command; invalid JSON")
+				continue
+			relayMemcacheCommand(mc_sock, command)
+			info['pos'] = float(eTime)
+		# Update the position after each batch
+		print("Updating position to %.6f" % info['pos'])
+		setCurrentPos(rd_host, info, config)
+		# Stop when there are no batches left
+		if len(events) < batchSize:
+			break
+
+	print("Done applying updates from redis server %s" % rd_host)
+
+def relayMemcacheCommand(mc_sock, command):
 	cmd = str(command['cmd']) # commands are always ASCII
 	key = str(command['key']) # keys are always ASCII
 
 	if cmd == 'set' or cmd == 'add':
+		# Apply value substitutions if requested
 		if 'sbt' in command and command['sbt']:
-			value = command['val'].replace('$UNIXTIME$', time.time())
-		else
+			value = command['val'].replace('$UNIXTIME$', '%.6f' % time.time())
+		else:
 			value = command['val']
-		mcCommand = "%s %s %s %s %s\r\n%s\r\n" % (cmd,key,command['flg'],command['ttl'],len(value),value)
+
+		mcCommand = "%s %s %s %s %s\r\n%s\r\n" % (
+				cmd,key,command['flg'],command['ttl'],len(value),value)
 	elif cmd == 'delete':
 		mcCommand = "delete %s\r\n" % key
 	else:
-		print('Got unrecognized command "%s"' % cmd)
-		return
+		raise Exception('BadMemcachedCommand', 'Got unrecognized command "%s"' % cmd)
 
-	print('Applying command: %s' % mcCommand)
+	print('Sending command: %s' % mcCommand)
 
 	# Issue the full command
-	mcSocket.sendall(mcCommand)
+	mc_sock.sendall(mcCommand)
 	# Get the response status (terminated with \r\n)
 	result = ''
 	while True:
-		c = mcSocket.recv(1)
+		c = mc_sock.recv(1)
 		if c == '\r' or c == '':
 			break
-		else:
-			result += c
-	mcSocket.recv(1) # consume \n
+		result += c
+	mc_sock.recv(1) # consume \n
 
 	# Check if the response was OK
 	if result in ['STORED','NOT_STORED','DELETED','NOT_FOUND']:
@@ -109,6 +184,32 @@ def relayCommand(mcSocket, message):
 	else:
 		raise Exception('BadMemcachedResult', 'Got bad result: %s' % result)
 
+	return result
+
+def getCurrentPos(rd_host, config):
+	log_path = os.path.join(
+		config['data_directory'], '%s:%s.pos' % (rd_host, config['redis_port']))
+
+	try:
+		f = open(log_path)
+		info = json.load(f)
+		f.close()
+	except IOError as e:
+		info = {'pos': 0.0}
+		print("Position file not found or not available")
+	except ValueError as e:
+		info = {'pos': 0.0}
+		print("Position file is not valid JSON")
+
+	return info
+
+def setCurrentPos(rd_host, info, config):
+	log_path = os.path.join(
+		config['data_directory'], '%s:%s.pos' % (rd_host, config['redis_port']))
+
+	f = open(log_path,'w')
+	f.write(json.dumps(info))
+	f.close()
 
 if __name__ == '__main__':
 	main()
