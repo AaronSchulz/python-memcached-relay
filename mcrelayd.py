f2d286c4d3bb34354389529df7dfcef0cbad2781
Aaron Schulz <aschulz@wikimedia.org>
2015-02-05 15:44:56 -0800

    Removed incr/decr support, since they are not idempotent
    Also split up some functions

--- mcrelayd.py
+++ mcrelayd.py
@@ -6,17 +6,9 @@ import os
 import yaml
 import redis
 
-incr_script = """
-if redis.call('EXISTS', KEYS[1]) == 1 then
-	return redis.call('INCR', KEYS[1], ARGV[1])
-end
-return false"""
-
-decr_script = """
-if redis.call('EXISTS', KEYS[1]) == 1 then
-	return redis.call('DECR', KEYS[1], ARGV[1])
-end
-return false"""
+rd_handles = {} # map of (redis host => StrictRedis)
+rd_ps_handles = {} # map of (redis host => RedisPubSub)
+rd_fail_times = {} # map of (redis host => UNIX timestamp)
 
 def main():
 	parser = argparse.ArgumentParser(description='Process memcached commands from a channel')
@@ -25,28 +17,13 @@ def main():
 
 	print("Loading YAML config...")
 	config = loadConfig(args.config_file)
+	print("Done")
 
 	# Connect to the memcached/redis cache server...
 	print("Connecting to local %s server..." % config['cache_type'])
-	target = None # either StrictRedis or Socket
-	if config['cache_type'] == 'memcached':
-		target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		target.connect((config['memcached_host'], config['memcached_port']))
-	elif config['cache_type'] == 'redis':
-		target = redis.StrictRedis(
-			host=config['redis_host'],
-			port=config['redis_port'],
-			password=config['redis_password'],
-			socket_connect_timeout=2,
-			socket_timeout=2)
-		target.ping()
-	else:
-		raise Exception('InvalidConfig', 'Invalid "cache_type" config')
+	target = getTargetCacheHandle(config) # either StrictRedis or Socket
 	print("Connected")
 
-	rd_handles = {} # map of (redis host => StrictRedis)
-	rd_ps_handles = {} # map of (redis host => RedisPubSub)
-	rd_fail_times = {} # map of (redis host => UNIX timestamp)
 	last_pos_write = {} # map of (redis host => UNIX timestamp)
 
 	# Connect to all the redis PubSub servers...
@@ -63,13 +40,13 @@ def main():
 		# Actually connect and subscribe (connection is on first command)
 		try:
 			# Sync from the reliable stream to the bulk of events
-			resyncViaRedisStream(target, rd_host, rd_handles, time.time(), config)
+			resyncViaRedisStream(target, rd_host, time.time(), config)
 			# Subscribe to channel to avoid polling overhead
-			print("Subscribing to channel %s on %s" % (config['redis_channel'],rd_host))
+			print("Subscribing to channel %s on %s..." % (config['redis_channel'],rd_host))
 			rd_ps_handles[rd_host].subscribe(config['redis_channel'])
 			print("Subscribed")
 			# Quikly resync to avoid any stream gaps (replay a few things twice)
-			resyncViaRedisStream(target, rd_host, rd_handles, time.time(), config)
+			resyncViaRedisStream(target, rd_host, time.time(), config)
 		except redis.RedisError as e:
 			rd_fail_times[rd_host] = time.time()
 			print("Error contacting redis server %s" % rd_host)
@@ -79,39 +56,18 @@ def main():
 	# Stream in updates from the channel on all servers indefinitely...
 	print("Listening for channel events...")
 	while True:
-		wait = True
+		foundAny = False
 		# Iterate through each host serving the channel
 		for rd_host in rd_ps_handles:
 			try:
-				# Avoid down servers but re-connect periodically if possible
-				streamPing(target, rd_host, rd_handles, rd_ps_handles, rd_fail_times, config)
-				# Process the next message if one is ready
-				event = rd_ps_handles[rd_host].get_message()
-				# @note: events are of the format <UNIX timetamp>:<JSON>
-				if event and event['type'] == 'message':
-					wait = False
-					try:
-						eTime, eMsg = event['data'].split(":",1)
-						command = json.loads(eMsg)
-					except ValueError as e:
-						print("Cannot relay command; invalid JSON")
-						continue
-					# Replicate the update to the memcached/redis server
-					if config['cache_type'] == 'redis':
-						relayRedisCommand(target, command)
-					else:
-						relayMemcacheCommand(target, command)
-					# Periodically update the position
-					cur_time = time.time();
-					if (cur_time - last_pos_write[rd_host]) > config['pos_write_delay']:
-						info = {'pos': float(eTime)}
-						set_current_pos(rd_host, info, config)
-						last_pos_write[rd_host] = cur_time
+				# If a relay command is ready then run it on the cache
+				gotCmd = relayPubSubMessage(target, rd_host, last_pos_write, config)
+				foundAny = gotCmd or foundAny
 			except redis.RedisError as e:
 				rd_fail_times[rd_host] = time.time()
 				print("Error contacting redis server %s" % rd_host)
-		# Avoid high CPU usage
-		if wait:
+		# Avoid high CPU usage when no commands were found
+		if not foundAny:
 			time.sleep(0.005)
 
 def loadConfig(config_file):
@@ -124,7 +80,52 @@ def loadConfig(config_file):
 
 	return config
 
-def streamPing(target, rd_host, rd_handles, rd_ps_handles, rd_fail_times, config):
+def getTargetCacheHandle(config):
+	if config['cache_type'] == 'memcached':
+		target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		target.connect((config['memcached_host'], config['memcached_port']))
+	elif config['cache_type'] == 'redis':
+		target = redis.StrictRedis(
+			host=config['redis_host'],
+			port=config['redis_port'],
+			password=config['redis_password'],
+			socket_connect_timeout=2,
+			socket_timeout=2)
+		target.ping()
+	else:
+		raise Exception('InvalidConfig', 'Invalid "cache_type" config')
+
+	return target
+
+def relayPubSubMessage(target, rd_host, last_pos_write, config):
+	# Avoid down servers but re-connect periodically if possible
+	redisStreamPing(target, rd_host, config)
+	# Process the next message if one is ready
+	event = rd_ps_handles[rd_host].get_message()
+	# @note: events are of the format <UNIX timetamp>:<JSON>
+	if event and event['type'] == 'message':
+		try:
+			eTime, eMsg = event['data'].split(":", 1)
+			command = json.loads(eMsg)
+		except ValueError as e:
+			print("Cannot relay command; invalid JSON")
+			return True
+		# Replicate the update to the memcached/redis server
+		if config['cache_type'] == 'redis':
+			relayRedisCommand(target, command)
+		else:
+			relayMemcacheCommand(target, command)
+		# Periodically update the position file
+		cur_time = time.time();
+		if (cur_time - last_pos_write[rd_host]) > config['pos_write_delay']:
+			info = {'pos': float(eTime)}
+			set_current_pos(rd_host, info, config)
+			last_pos_write[rd_host] = cur_time
+		return True
+	else:
+		return False
+
+def redisStreamPing(target, rd_host, config):
 	if not rd_host in rd_fail_times:
 		return
 	if (time.time() - rd_fail_times[rd_host]) >= config['retry_timeout']:
@@ -134,9 +135,9 @@ def streamPing(target, rd_host, rd_handles, rd_ps_handles, rd_fail_times, config
 		del rd_fail_times[rd_host]
 		print("Subscribed")
 		# Resync from the reliable stream (replay a few things twice)
-		resyncViaRedisStream(target, rd_host, rd_handles, time.time(), config)
+		resyncViaRedisStream(target, rd_host, time.time(), config)
 
-def resyncViaRedisStream(target, rd_host, rd_handles, stopPos, config):
+def resyncViaRedisStream(target, rd_host, stopPos, config):
 	# Prefix the channel to get the stream key
 	key = "z-stream:%s" % config['redis_channel']
 
@@ -146,7 +147,7 @@ def resyncViaRedisStream(target, rd_host, rd_handles, stopPos, config):
 	info = get_current_pos(rd_host, config)
 	# Adjust time range to handle any clock skew
 	clockSkewFuzz = 5
-	info['pos'] = max( 0, info['pos'] - clockSkewFuzz )
+	info['pos'] = max(0, info['pos'] - clockSkewFuzz)
 	stopPos = stopPos + clockSkewFuzz
 
 	batchSize = 100
@@ -191,20 +192,18 @@ def relayMemcacheCommand(mc_sock, command):
 
 		# Apply value substitutions if requested
 		if command.get('sbt', None):
-			command['val'] = command['val'].replace('$UNIXTIME$', '%.6f' % time.time())
+			purgeTime = time.time() + command.get('uto', 0)
+			command['val'] = command['val'].replace('$UNIXTIME$', '%.6f' % purgeTime)
 
 		if cmd == 'set' or cmd == 'add':
 			mcCommand = "%s %s %s %s %s\r\n%s\r\n" % (cmd, key,
 				command['flg'], command['ttl'], len(command['val']), command['val'])
-		elif cmd == 'incr' or cmd == 'decr':
-			value = long(command['val'])
-			mcCommand = "%s %s %s\r\n" % (cmd,key,value)
 		elif cmd == 'delete':
 			mcCommand = "delete %s\r\n" % key
 		else:
 			print('Got unrecognized memcached command "%s"' % cmd)
 			return None
-	except (KeyError,ValueError) as e:
+	except (KeyError, ValueError) as e:
 		print('Got incomplete or invalid relay command')
 		return None
 
@@ -236,8 +235,9 @@ def relayRedisCommand(rd_handle, command):
 		print("Got '%s' relay command to key %s" % (cmd,key))
 
 		# Apply value substitutions if requested
-		if 'val' in command and 'sbt' in command and command['sbt']:
-			command['val'] = command['val'].replace('$UNIXTIME$', '%.6f' % time.time())
+		if command.get('sbt', None):
+			purgeTime = time.time() + command.get('uto', 0)
+			command['val'] = command['val'].replace('$UNIXTIME$', '%.6f' % purgeTime)
 
 		if cmd == 'set':
 			if command['ttl'] == 0:
@@ -252,16 +252,12 @@ def relayRedisCommand(rd_handle, command):
 					return rd_handle.setex(key, command['ttl'], command['val'])
 			else:
 				return False
-		elif cmd == 'incr':
-			return rd_handle.eval(incr_script, 1, key, command['val'])
-		elif cmd == 'decr':
-			return rd_handle.eval(decr_script, 1, key, command['val'])
 		elif cmd == 'delete':
 			return rd_handle.delete(key)
 		else:
 			print('Got unrecognized redis command "%s"' % cmd)
 			return None
-	except (KeyError,ValueError) as e:
+	except (KeyError, ValueError) as e:
 		print('Got incomplete or invalid relay command')
 		return None
 	except redis.RedisError as e:
